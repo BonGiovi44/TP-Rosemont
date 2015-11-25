a9bdfac8fdd36af6d74c54b338f637c286e40586
Gio <giodes@hotmail.ca>
2015-11-25 12:05:32 -0500

    Ajout de commentaires dans frame superviseur

--- FrameSuperviseur.java
+++ FrameSuperviseur.java
@@ -10,12 +10,12 @@ import java.awt.event.*;
 
 //class pour le menu du superviseur
 public class FrameSuperviseur extends JFrame{
-
 	
 	public FrameSuperviseur()
 	{
 		Superviseur unSuperviseur = new Superviseur();
 		setTitle("Superviseur - Gestidon");
+		//Precise la taille par default du frame sup.
 		setBounds(100, 100, 502, 371);
 		//Set la taille du frame au maximum
 		setExtendedState(JFrame.MAXIMIZED_BOTH); 
@@ -27,24 +27,27 @@ public class FrameSuperviseur extends JFrame{
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.BOTTOM);
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
+		//Creation d'un panel pour le premier onglet
 		JPanel panelEnr = new JPanel();
-		tabbedPane.addTab("Enregistrer.Emp", null, panelEnr, null);
-		GridBagLayout gbl_panelEnr = new GridBagLayout();
+		tabbedPane.addTab("Enregistrer Emp.", null, panelEnr, null);//Ajout du premier onglet dans son conteneur
+		GridBagLayout gbl_panelEnr = new GridBagLayout();//Creation d'un gridbag Layout pour le panel enr. emplo.
 		gbl_panelEnr.columnWidths = new int[]{137, 86, 280, 0};
 		gbl_panelEnr.rowHeights = new int[]{19, 20, 0, 20, 20, 20, 20, 20, 0};
 		gbl_panelEnr.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 		gbl_panelEnr.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panelEnr.setLayout(gbl_panelEnr);
 		
+		//Creation d'un label pour nom
 		JLabel lblNewLabel_1 = new JLabel("Nom");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);//On ajuste l'alignement 
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();//Creation de contraintes
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHEAST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 0;
-		panelEnr.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		panelEnr.add(lblNewLabel_1, gbc_lblNewLabel_1);//Ajoute le label dans le premier onglet
 		
+		//Creation d'un champ de texte pour le nom
 		JTextField champNom = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.fill = GridBagConstraints.BOTH;
@@ -54,6 +57,7 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champNom, gbc_textField);
 		champNom.setColumns(10);
 		
+		//Creation d'un label pour le prenom
 		JLabel lblNewLabel_2 = new JLabel("Prenom");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
@@ -63,6 +67,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblNewLabel_2.gridy = 1;
 		panelEnr.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
+		//Creation d'un champ de texte pour prenom
 		JTextField champPrenom = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.anchor = GridBagConstraints.NORTH;
@@ -73,7 +78,8 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champPrenom, gbc_textField_1);
 		champPrenom.setColumns(10);
 		
-		JLabel lblNumero = new JLabel("Numero");
+		//Creation d'un champ de texte pour le numero de telephone
+		JLabel lblNumero = new JLabel("Numero de telephone");
 		GridBagConstraints gbc_lblNumero = new GridBagConstraints();
 		gbc_lblNumero.anchor = GridBagConstraints.EAST;
 		gbc_lblNumero.insets = new Insets(0, 0, 5, 5);
@@ -81,6 +87,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblNumero.gridy = 2;
 		panelEnr.add(lblNumero, gbc_lblNumero);
 		
+		//Creation d'un champ de texture pour le numero de telephone
 		JTextField champNumero = new JTextField();
 		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
 		gbc_textField_7.insets = new Insets(0, 0, 5, 5);
@@ -90,6 +97,7 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champNumero, gbc_textField_7);
 		champNumero.setColumns(10);
 		
+		//Creation d'un label pour l'adresse
 		JLabel lblAdresse = new JLabel("Adresse");
 		lblAdresse.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblAdresse = new GridBagConstraints();
@@ -99,6 +107,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblAdresse.gridy = 3;
 		panelEnr.add(lblAdresse, gbc_lblAdresse);
 		
+		//Creation d'un champ de texture pour l'adresse
 		JTextField champAdresse = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.anchor = GridBagConstraints.NORTH;
@@ -109,6 +118,7 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champAdresse, gbc_textField_2);
 		champAdresse.setColumns(10);
 		
+		//Creation d'un label pour le couriel
 		JLabel lblNewLabel_3 = new JLabel("Couriel");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
@@ -117,6 +127,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblNewLabel_3.gridy = 4;
 		panelEnr.add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
+		//Creation d'un champ de texture pour le couriel
 		JTextField champCouriel = new JTextField();
 		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
 		gbc_textField_3.anchor = GridBagConstraints.NORTH;
@@ -127,6 +138,7 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champCouriel, gbc_textField_3);
 		champCouriel.setColumns(10);
 		
+		//Creation d'un label pour les heures travaille
 		JLabel lblHeuresTravailles = new JLabel("Heures travaill\u00E9es");
 		GridBagConstraints gbc_lblHeuresTravailles = new GridBagConstraints();
 		gbc_lblHeuresTravailles.anchor = GridBagConstraints.EAST;
@@ -135,6 +147,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblHeuresTravailles.gridy = 5;
 		panelEnr.add(lblHeuresTravailles, gbc_lblHeuresTravailles);
 		
+		//Creation d'un champ de texture pour les heures travaille
 		JTextField champHT = new JTextField();
 		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
 		gbc_textField_5.anchor = GridBagConstraints.NORTH;
@@ -145,6 +158,7 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champHT, gbc_textField_5);
 		champHT.setColumns(10);
 		
+		//Creation d'un label pour le taux horaire
 		JLabel lblTauxHoraire = new JLabel("Taux Horaire");
 		GridBagConstraints gbc_lblTauxHoraire = new GridBagConstraints();
 		gbc_lblTauxHoraire.anchor = GridBagConstraints.EAST;
@@ -153,6 +167,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblTauxHoraire.gridy = 6;
 		panelEnr.add(lblTauxHoraire, gbc_lblTauxHoraire);
 		
+		//Creation d'un champ de texture pour le taux horaire
 		JTextField champTH = new JTextField();
 		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
 		gbc_textField_6.anchor = GridBagConstraints.NORTH;
@@ -163,6 +178,7 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champTH, gbc_textField_6);
 		champTH.setColumns(10);
 		
+		//Creation d'un label pour le salaire
 		JLabel lblSalire = new JLabel("Salaire");
 		GridBagConstraints gbc_lblSalire = new GridBagConstraints();
 		gbc_lblSalire.anchor = GridBagConstraints.EAST;
@@ -171,6 +187,7 @@ public class FrameSuperviseur extends JFrame{
 		gbc_lblSalire.gridy = 7;
 		panelEnr.add(lblSalire, gbc_lblSalire);
 		
+		//Creation d'un champ de texture pour le salaire
 		JTextField champSalaire = new JTextField();
 		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
 		gbc_textField_4.insets = new Insets(0, 0, 0, 5);
@@ -181,19 +198,22 @@ public class FrameSuperviseur extends JFrame{
 		panelEnr.add(champSalaire, gbc_textField_4);
 		champSalaire.setColumns(10);
 		
+		
+		//Creation elements Deuxieme onglet(Employes)
 		JPanel panel_Emp = new JPanel();
-		tabbedPane.addTab("Employes", null, panel_Emp, null);
-		panel_Emp.setLayout(new BorderLayout(0, 0));
+		tabbedPane.addTab("Employes", null, panel_Emp, null);//Conteneur pour l'onglet employe
+		panel_Emp.setLayout(new BorderLayout(0, 0));//Ajout d'un border layout a l'onglet employe
 		
 		JLabel label_emp = new JLabel("Informations Employe");
 		panel_Emp.add(label_emp, BorderLayout.NORTH);
 		
-		JScrollPane emp_scrollPane = new JScrollPane();
+		JScrollPane emp_scrollPane = new JScrollPane();//Creation d'une barre de défilement en cas d'un d'debordement des informations
 		panel_Emp.add(emp_scrollPane, BorderLayout.CENTER);
 		
 		JTextPane emp_textPane = new JTextPane();
 		emp_scrollPane.setViewportView(emp_textPane);
 		
+		//Creation elements troisieme onglet(Benevoles)
 		JPanel panel_Ben = new JPanel();
 		tabbedPane.addTab("Benevoles", null, panel_Ben, null);
 		panel_Ben.setLayout(new BorderLayout(0, 0));
@@ -207,6 +227,7 @@ public class FrameSuperviseur extends JFrame{
 		JTextPane ben_textPane = new JTextPane();
 		ben_scrollPane.setViewportView(ben_textPane);
 		
+		//Creation elements quatrieme onglet(Donateurs)
 		JPanel panel_Don = new JPanel();
 		tabbedPane.addTab("Donateurs", null, panel_Don, null);
 		panel_Don.setLayout(new BorderLayout(0, 0));
@@ -220,14 +241,17 @@ public class FrameSuperviseur extends JFrame{
 		JTextPane don_textPane = new JTextPane();
 		don_scrollPane.setViewportView(don_textPane);
 		
+		//Creation d'un label qui sert de barre de status 
 		JLabel lblNewLabel = new JLabel("GestiDon - Status:");
-		getContentPane().add(lblNewLabel, BorderLayout.SOUTH);
+		getContentPane().add(lblNewLabel, BorderLayout.SOUTH);//Contrainte pour que le label reste au sud
 		
+		//Creation d'une barre d'outil
 		JToolBar toolBar = new JToolBar();
 		getContentPane().add(toolBar, BorderLayout.NORTH);
-		
+		//Creation du premier bouton de la barre d'outril(Enregistrer)
 		JButton btnEnregistrer = new JButton("Enregistrer");
-		btnEnregistrer.addActionListener(new ActionListener() {
+		//Gestionnaire d'evenement du bouton enregistrer
+		btnEnregistrer.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
 				unSuperviseur.EnregistrerEmploye(champNom.getText(),
@@ -248,8 +272,9 @@ public class FrameSuperviseur extends JFrame{
 		
 		Component horizontalStrut = Box.createHorizontalStrut(20);
 		toolBar.add(horizontalStrut);
-		
+		//Creation du deuxieme bouton
 		JButton btnLister = new JButton("Lister");
+		//Gestionnaire d'evenement du bouton lister
 		btnLister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				emp_textPane.setText(Gestion.getListeEmploye());
@@ -257,14 +282,15 @@ public class FrameSuperviseur extends JFrame{
 				don_textPane.setText(Gestion.getListeDonateur());
 			}
 		});
-		toolBar.add(btnLister);
+		toolBar.add(btnLister);//Ajout du bouton lister a la barre d'outil
 		
+		//Creation d'une barre de menu
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+		//Creation du bouton menu Fichier
 		JMenu mnNewMenu = new JMenu("Fichier");
 		menuBar.add(mnNewMenu);
-		
+		//Creation  de l'option acceuil de menu fichier
 		JMenuItem mntmMenu = new JMenuItem("Accueil");
 		mntmMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -273,15 +299,15 @@ public class FrameSuperviseur extends JFrame{
 			}
 		});
 		mnNewMenu.add(mntmMenu);
-		
+		//Creation de l'option quitter du menu fichier
 		JMenuItem mntmQuitter = new JMenuItem("Quitter");
 		mntmQuitter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				java.lang.System.exit(0);
+				java.lang.System.exit(0);//Permet de quitter le programme en toute sécuriter! 
 			}
 		});
 		mnNewMenu.add(mntmQuitter);
-		//promt on close
+		//Permet de verifier si on veut vraiment quitter
 		this.addWindowListener( new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent we) {
